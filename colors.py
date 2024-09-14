8bf58f3d61b91b2890f977c6ad10c76ac2ec75a2
Niklas Lutze <me@niklaslutze.de>
2024-09-15 00:44:12 +0200

    style plots

---
+++ colors.py
@@ -0,0 +1,16 @@
+GRASS = "#327852"
+GRASS_D = "#354537"
+GRASS_B = "#7BAD92"
+GRAY = "#A6B2C1"
+GRAY_D = "#546478"
+BROWNISH = "#8f4e44"
+BLUE_GRAY = "#aeb5d6"
+WHALE = "#4a588a"
+GOLD = "#c9c343"
+
+
+MATCH_RESULT = {
+    "host_wins": GRASS,
+    "guest_wins": "#AA5D47",
+    "draw": GRAY
+}
--- model/plots.py
+++ model/plots.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from cycler import cycler
 
-def cross_val_plot(df_matches, splits, labels, scores):
+def cross_val_plot(df_matches, splits, labels, scores, color=None):
     scores = np.array(scores)
     sorting = np.argsort(-np.mean(scores, axis=1))
     labels = [labels[i] for i in sorting]
     scores = scores[sorting]
+    if color is not None:
+        color = np.array(color)[sorting]
 
     for label, score in zip(labels, scores):
         print(f"{label}:\t{score.mean():.3f} +- {score.std():.3f}")
@@ -13,14 +16,22 @@ def cross_val_plot(df_matches, splits, labels, scores):
     fig, axs = plt.subplots(ncols=2, figsize=(12, 4))
     split_test_seasons = [df_matches.iloc[split[1][0]]["season"] for  split in splits]
 
+    if color is not None:
+        axs[0].set_prop_cycle(cycler(color=color))
     axs[0].plot(scores.T, label=labels)
     axs[0].set_xticks(range(len(split_test_seasons)), split_test_seasons, rotation=90, ha='center')
-    axs[0].set_xlabel("Test season")
-    axs[0].set_ylabel("Score")
+    axs[0].set_xlabel("Test Saison")
+    axs[0].set_ylabel("Punkte")
     axs[0].legend()
 
-    axs[1].boxplot(scores.T)
-    axs[1].set_xlabel("Strategy")
+    if color is not None:
+        bplot = axs[1].boxplot(scores.T, patch_artist=True)
+        for patch, color in zip(bplot['boxes'], color):
+            patch.set_facecolor(color)
+    else:
+        bplot = axs[1].boxplot(scores.T)
+
+    axs[1].set_xlabel("Model")
     axs[1].set_xticks(range(1, len(labels) +1), labels, rotation=45, ha="center")
     fig.tight_layout()
     return fig, axs
